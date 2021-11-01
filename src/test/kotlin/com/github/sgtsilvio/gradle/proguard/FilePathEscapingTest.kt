7e1c3e15b982c1f46dfa7c834fe45e5eb1d5fe30
Silvio Giebl <gieblsilvio@outlook.com>
2021-11-01 13:06:59 +0100

    Add FilePathEscapingTest

--- src/test/kotlin/com/github/sgtsilvio/gradle/proguard/FilePathEscapingTest.kt
+++ src/test/kotlin/com/github/sgtsilvio/gradle/proguard/FilePathEscapingTest.kt
@@ -3,7 +3,6 @@ package com.github.sgtsilvio.gradle.proguard
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
@@ -11,10 +10,13 @@ import java.io.File
 /**
  * @author Silvio Giebl
  */
-class ConfigurationCacheTest {
+class FilePathEscapingTest {
 
     @Test
-    fun configurationCacheReused(@TempDir projectDir: File) {
+    fun spaceAndParenthesisAreEscaped(@TempDir tempDir: File) {
+        val projectDir = tempDir.resolve("wa bern(test)")
+        projectDir.mkdir()
+
         projectDir.resolve("settings.gradle.kts").writeText(
             """
             rootProject.name = "test"
@@ -33,15 +35,12 @@ class ConfigurationCacheTest {
                 inJars(tasks.jar)
                 libraryJars("${'$'}{System.getProperty("java.home")}/jmods/java.base.jmod", "!**.jar;!module-info.class")
                 outJars(base.libsDirectory.file("test-proguarded.jar"))
+                configurationFile.set(layout.buildDirectory.file("test-config.txt"))
                 mappingFile.set(layout.buildDirectory.file("test-mapping.txt"))
+                seedsFile.set(layout.buildDirectory.file("test-seeds.txt"))
+                usageFile.set(layout.buildDirectory.file("test-usage.txt"))
+                dumpFile.set(layout.buildDirectory.file("test-dump.txt"))
                 rules.add("-keep class test.Main { public static void main(java.lang.String[]); }")
-            }
-            // copy inJars, libraryJars, outJars to check if they are compatible with the configuration cache
-            val copyProguardJars by tasks.registering(Copy::class) {
-                from(proguardJar.map { it.inJars }) { into("inJars") }
-                from(proguardJar.map { it.libraryJars }) { into("libraryJars") }
-                from(proguardJar.map { it.outJars }) { into("outJars") }
-                into(layout.buildDirectory.dir("copyProguardJars"))
             }
             """.trimIndent()
         )
@@ -59,25 +58,10 @@ class ConfigurationCacheTest {
         val result = GradleRunner.create()
             .withProjectDir(projectDir)
             .withPluginClasspath()
-            .withArguments("copyProguardJars", "--configuration-cache")
+            .withArguments("proguardJar")
             .build()
 
-        assertTrue(result.output.contains("Configuration cache entry stored"))
         assertEquals(TaskOutcome.SUCCESS, result.task(":jar")?.outcome)
         assertEquals(TaskOutcome.SUCCESS, result.task(":proguardJar")?.outcome)
-        assertEquals(TaskOutcome.SUCCESS, result.task(":copyProguardJars")?.outcome)
-
-        projectDir.resolve("build").deleteRecursively()
-
-        val result2 = GradleRunner.create()
-            .withProjectDir(projectDir)
-            .withPluginClasspath()
-            .withArguments("copyProguardJars", "--configuration-cache")
-            .build()
-
-        assertTrue(result2.output.contains("Configuration cache entry reused"))
-        assertEquals(TaskOutcome.SUCCESS, result2.task(":jar")?.outcome)
-        assertEquals(TaskOutcome.SUCCESS, result2.task(":proguardJar")?.outcome)
-        assertEquals(TaskOutcome.SUCCESS, result2.task(":copyProguardJars")?.outcome)
     }
 }
